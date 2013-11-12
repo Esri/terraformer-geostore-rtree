4302c698d005d4348202c1fa49fb61aefa6798f6
Patrick Arlt <patrick.arlt@gmail.com>
2013-11-12 14:09:08 -0800

    add bower.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function (grunt) {
   grunt.initConfig({
+    aws: grunt.file.readJSON(process.env.HOME + '/terraformer-s3.json'),
     pkg:   grunt.file.readJSON('package.json'),
 
     meta: {
@@ -26,6 +27,11 @@ module.exports = function (grunt) {
       "terraformer-geostore-rtree": {
         src: ["terraformer-geostore-rtree.js"],
         dest: 'terraformer-geostore-rtree.min.js'
+      },
+
+      version: {
+        src: ['terraformer-geostore-rtree.js'],
+        dest: 'versions/terraformer-geostore-rtree-<%= pkg.version %>.min.js'
       }
     },
 
@@ -68,6 +74,28 @@ module.exports = function (grunt) {
           maintainability: 65
         }
       }
+    },
+
+    s3: {
+      options: {
+        key: '<%= aws.key %>',
+        secret: '<%= aws.secret %>',
+        bucket: '<%= aws.bucket %>',
+        access: 'public-read',
+        headers: {
+          // 1 Year cache policy (1000 * 60 * 60 * 24 * 365)
+          "Cache-Control": "max-age=630720000, public",
+          "Expires": new Date(Date.now() + 63072000000).toUTCString()
+        }
+      },
+      dev: {
+        upload: [
+          {
+            src: 'versions/terraformer-geostore-rtree-<%= pkg.version %>.min.js',
+            dest: 'terraformer-geostore-rtree/<%= pkg.version %>/terraformer-geostore-rtree.min.js'
+          }
+        ]
+      }
     }
   });
 
@@ -75,7 +103,9 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-complexity');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-s3');
 
   grunt.registerTask('test', ['jasmine']);
   grunt.registerTask('default', [ 'jshint', 'jasmine', 'uglify', 'complexity' ]);
+  grunt.registerTask('version', ['default', 's3']);
 };
